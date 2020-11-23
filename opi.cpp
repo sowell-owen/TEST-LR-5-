744e723c30bc1551bba10ddf4c1a844e1939b813
sowell-owen <karakulin.toshik@gmail.com>
2020-11-23 20:47:45 +0200

    laboratorka 6

--- opi.cpp
+++ opi.cpp
@@ -1,11 +1,9 @@
-﻿#include <iostream>
-#include "windows.h"
-
+﻿#include "stdio.h"
+using namespace std;
 int main()
 {
-	using namespace std;
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
-	cout << "Привіт, Антон Каракулін\n";
+	int k = 3, m;
+	m = 2;
+	double R = 4 + k - m;
+	printf("R = %lf", R);
 }
