da01e29b32c154567c2eca60e7ee3157ac3de866
sowell-owen <karakulin.toshik@gmail.com>
2020-12-16 12:16:20 +0200

    new file test added

---
+++ test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr;
+            string myStr2;
+
+            int a, b;
+            
+            Console.WriteLine("Введите числа: ") ;
+
+            myStr = Console.ReadLine();
+            myStr2 = Console.ReadLine();
+          
+
+            a = Convert.ToInt32(myStr);
+            b = Convert.ToInt32(myStr2);
+
+
+            int result = (a + b) / 2;
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
